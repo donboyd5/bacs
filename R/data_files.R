5fefa55d04b927674c5df085d4cb1c06c3c6a736
Don Boyd <donboyd5@gmail.com>
2023-02-28 04:55:41 -0500

    document acsvars

--- R/data_files.R
+++ R/data_files.R
@@ -11,3 +11,16 @@
 #' acstabs
 "acstabs"
 
+
+#' ACS variables metadata
+#'
+#' @source Constructed from Census documentation <https://www.census.gov/programs-surveys/acs/technical-documentation/table-shells.html>.
+#'
+#' @format
+#' Tibble with 1 row per ACS variable, with columns table, line, variable, label, release, year, title, universe,
+#' uvariable, where uvariable is a uniform variable name (experiemental) that puts an underscore in names from early years,
+#' which do not have underscores.
+#'
+#' @examples
+#' acsvars
+"acsvars"
